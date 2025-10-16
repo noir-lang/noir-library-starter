829c0bcc208213d921322c24c2c9a5afb15968d0
github-action-benchmark <github@users.noreply.github.com>
2025-10-16 14:54:59 +0000

    add Circuit Size (customSmallerIsBetter) benchmark result for 80e69c8c237b110b2f243630b91f064aed32bc46

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760626498220,
+  "lastUpdate": 1760626499360,
   "repoUrl": "https://github.com/noir-lang/noir-library-starter",
   "entries": {
     "ACIR Opcodes": [
@@ -34,6 +34,42 @@ window.BENCHMARK_DATA = {
             "name": "prop_mul.json/main",
             "value": 1,
             "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ],
+    "Circuit Size": [
+      {
+        "commit": {
+          "author": {
+            "email": "26174818+jialinli98@users.noreply.github.com",
+            "name": "Jialin Li",
+            "username": "jialinli98"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80e69c8c237b110b2f243630b91f064aed32bc46",
+          "message": "chore: update fuzz test (#22)\n\n* updated fuzz test and benchmark templates",
+          "timestamp": "2025-10-16T16:54:34+02:00",
+          "tree_id": "5d603769326bc7e7722091365d79337507c2543f",
+          "url": "https://github.com/noir-lang/noir-library-starter/commit/80e69c8c237b110b2f243630b91f064aed32bc46"
+        },
+        "date": 1760626499347,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_test.json/main",
+            "value": 116,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "prop_mul.json/main",
+            "value": 17,
+            "unit": "circuit_size"
           }
         ]
       }
